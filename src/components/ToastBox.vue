1e6f8fb168bd12fa5f672f98f21469fdf917e579
KimJY <kjy3501@naver.com>
2022-09-28 13:07:38 +0900

    내용수정 작업

---
+++ src/components/ToastBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert toast-box" :class="`alert=${color}`" role="alert">
+    <!-- Success -->
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["message"],
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "success",
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.toast-box {
+  position: fixed;
+  top: 10px;
+  right: 10px;
+}
+</style>
